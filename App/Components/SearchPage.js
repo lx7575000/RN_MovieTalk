7c4ec5ec9a9ac2590a7e3537fd046b0bfc81b35a
lx7575000 <liu_narcisus@163.com>
2016-02-16 23:39:05 +0800

    add new Function in SearchPage about re-search the history item

--- App/Components/SearchPage.js
+++ App/Components/SearchPage.js
@@ -39,10 +39,29 @@ export default class SearchPage extends Component{
   }
 
   /*
+    添加新的搜索记录
+  */
+  searchHistory(){
+    /*
+      1. 将查询元素放入新数组，
+      2. 然后通过创建新Set去掉数组内重复元素，
+      3. 最后再通过spread操作符将元素再转化为数组
+    */
+    let newSearchHistory = [...new Set([this.state.query, ...this.state.searchHistory])];
+
+    this.setState({
+      searchHistory: newSearchHistory
+    });
+  }
+
+
+  /*
     获取搜索电影的数据
   */
-
   fetchData(){
+    //调用添加新搜索记录
+    this.searchHistory();
+
     this.setState({
       loaded: false,
       opacity: 1,
@@ -70,13 +89,30 @@ export default class SearchPage extends Component{
   }
 
   /*
+    搜索电影具体信息
+      此处用于重新查询搜索历史内容
+    应用ES7的async实现异步
+  */
+  async search(item){
+    try{
+      //当await的内容执行完成后才会继续执行后面操作
+      await this.setState({
+        query: item
+      });
+      this.fetchData();
+    }catch(e){
+      console.log(e);
+    }
+  }
+
+  /*
     具体的各个搜索历史单元页面
   */
   renderSearchHistoryList(item){
     return(
       <TouchableHighlight
         underlayColor='rgba(34, 26, 38, 0.1)'
-        onPress={ () => {}}
+        onPress={ () => this.search(item)}
         >
         <View style={styles.item}>
           <View style={styles.itemContent}>
@@ -92,7 +128,6 @@ export default class SearchPage extends Component{
   /*
     通过使用 loaded设定 ActivityIndicatorIOS中的animating状态，
     然后设定opacity属性值设定ActivityIndicatorIOS组件是否可见
-
   */
   render(){
     return (
@@ -102,6 +137,7 @@ export default class SearchPage extends Component{
             第一部分  --- 搜索框
           */}
           <TextInput
+            value={this.state.query}
             style={{height: 50}}
             placeholder="搜索..."
             autoFocus={true}
@@ -109,6 +145,7 @@ export default class SearchPage extends Component{
             returnKeyType="search"
             placeholderTextColor="#6435c9"
             clearButtonMode="while-editing"
+            clearTextOnFocus={true}
             onChangeText={ (query) => {
               this.setState({
                 query
